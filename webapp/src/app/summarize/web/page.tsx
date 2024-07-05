566b0f4b0f65eca2e53e66fc2483b407770f3fbc
J Shamsul Bahri <jibone@gmail.com>
2024-07-05 17:34:52 +0800

    Implemented text summerisation interface. (#5)

---
+++ webapp/src/app/summarize/web/page.tsx
@@ -0,0 +1,15 @@
+import Layout from "@/ui/Layout";
+
+export default function SummarizeWeb() {
+  return (
+    <Layout title="Summarize Webpage" sideNavItem="Summarize">
+      <div className="prose prose-slate">
+        <p>Work in progress</p>
+        <p>
+          An interface where you could paste a public URL and a summary of the
+          page can be written up.
+        </p>
+      </div>
+    </Layout>
+  );
+}
