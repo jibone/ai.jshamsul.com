eecf70cc35f40a4f0479844bb42688dbedcebc7b
Jibone <jibone@gmail.com>
2024-11-20 11:04:06 +0800

    Move config file and add melaka and mat rock

---
+++ webapp/src/config/sembangSystemPrompt.ts
@@ -0,0 +1,62 @@
+export const sembangSystemPrompt = [
+  {
+    label: "Malay",
+    prompt: "dengan bahasa melayu",
+  },
+  {
+    label: "Chinese",
+    prompt: "dengan bahasa cina",
+  },
+  {
+    label: "Tamil",
+    prompt: "dengan bahasa tamil",
+  },
+  {
+    label: "Jawi",
+    prompt: "dengahn bahasa jawi",
+  },
+  {
+    label: "Loghat Perak",
+    prompt: "dengan loghat bahasa Perak",
+  },
+  {
+    label: "Loghat Kelantan",
+    prompt: "dengan loghat bahasa Kelantan",
+  },
+  {
+    label: "Loghat Terengganu",
+    prompt: "dengan loghat bahasa Terengganu",
+  },
+  {
+    label: "Loghat Melaka",
+    prompt: "dengan loghat bahasa Malaka baba nyonya",
+  },
+  {
+    label: "Bahasa Sabah",
+    prompt: "dengan bahasa Sabah",
+  },
+  {
+    label: "Mat Rempit",
+    prompt: "macam mat rempit",
+  },
+  {
+    label: "Mat Rockers",
+    prompt: "macam mat rockers yang selalu dengar lagu rock kapak",
+  },
+  {
+    label: "Gangster",
+    prompt: "macam gangster",
+  },
+  {
+    label: "Ahli Politik",
+    prompt: "macam ahli politik sedang berkempen",
+  },
+  {
+    label: "Karyawan",
+    prompt: "dengan berpantun",
+  },
+  {
+    label: "Singlish",
+    prompt: "dengan bahasa Singlish",
+  },
+];
